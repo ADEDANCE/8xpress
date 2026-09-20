9291883b7a71e4c7c3914588d9a6b49bc1ed01f2
ADEDANCE <uthmanadedance@gmail.com>
2026-09-20 05:16:41 +0100

    import category tab

--- src/app/(tabs)/menu.tsx
+++ src/app/(tabs)/menu.tsx
@@ -1,9 +1,53 @@
-import { View, Text } from 'react-native';
+import { colors } from "@/constants/colors";
+import { Pressable, ScrollView, StyleSheet, Text } from "react-native";
+import CategoryTab from "../screens/home/CategoryTab";
+import MenuCard from "../screens/home/MenuCard";
 
 export default function Menu() {
   return (
-    <View>
-      <Text>Menu</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Text style={styles.pageTitle}>THE MENU</Text>
+      <Text style={styles.pageDescription}>Fresh. Hot. Made to order.</Text>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        <Pressable>
+          <CategoryTab title={"All"} />
+        </Pressable>
+        <Pressable>
+          <CategoryTab title={"Party Jollof"} />
+        </Pressable>
+        <Pressable>
+          <CategoryTab title={"Shawarma"} />
+        </Pressable>
+        <Pressable>
+          <CategoryTab title={"Add-ons"} />
+        </Pressable>
+        <Pressable>
+          <CategoryTab title={"Drinks"} />
+        </Pressable>
+      </ScrollView>
+
+      <MenuCard />
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000000",
+    paddingHorizontal: 24,
+    paddingBottom: 40,
+  },
+
+  pageTitle: {
+    fontWeight: "bold",
+    fontSize: 30,
+    marginTop: 8,
+    color: colors.white,
+  },
+
+  pageDescription: {
+    color: colors.gray,
+    fontSize: 15,
+  },
+});
