4f4e48c2e89422c93a3405f40ce6ed18a7bedc26
ADEDANCE <uthmanadedance@gmail.com>
2026-09-23 15:45:10 +0100

    Feat:Create delivary info route

---
+++ src/app/delivarydetails/DelivaryDetailScreen.tsx
@@ -0,0 +1,7 @@
+import DelivaryDetailScreen from "../screens/ordertype/DelivaryDetailScreen";
+
+const DeliveryDetail = () => {
+  return <DelivaryDetailScreen />;
+};
+
+export default DeliveryDetail;
